a273457b8eb9748d22f017fed237d3786f9c211c
Suryansh Goyal <sgoyal2402@gmail.com>
2020-06-10 12:13:31 +0530

    Level 2 Encryption

---
+++ app.js
@@ -0,0 +1,78 @@
+//jshint esversion:6
+const express = require('express');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const url = "mongodb://localhost:27017/userDB";
+const options = {
+useNewUrlParser: true,
+useUnifiedTopology: true
+};
+
+mongoose.connect(url, options);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields:['password']});
+
+const User = mongoose.model("User", userSchema);
+
+
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const user = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    user.save((err)=> {
+        if(err)
+        console.log(err);
+        else
+        res.render("secrets");
+    });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, (err, doc) => {
+        if(err)
+        console.log(err);
+        else{
+            if(doc && doc.password === password){
+                res.render("secrets");
+            }
+        }
+        
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log("Server running successfully on port 3000");
+    
+});
