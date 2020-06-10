70ec68cad7c30c1fb77af042e52d8533dd011f1f
Suryansh Goyal <sgoyal2402@gmail.com>
2020-06-10 17:13:34 +0530

    Level 4 bcrypt salting and hashing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const url = "mongodb://localhost:27017/userDB";
 const options = {
@@ -43,28 +44,34 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const user = new User({
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      // Store hash in your password DB.
+      const user = new User({
         email: req.body.username,
-        password: md5(req.body.password)
+        password: hash,
+      });
+      user.save((err) => {
+        if (err) console.log(err);
+        else res.render("secrets");
+      });
     });
-    user.save((err)=> {
-        if(err)
-        console.log(err);
-        else
-        res.render("secrets");
-    });
+    
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, (err, doc) => {
         if(err)
         console.log(err);
         else{
-            if(doc && doc.password === password){
-                res.render("secrets");
+            if(doc){
+                bcrypt.compare(password, doc.password, function (err, result) {
+                  if(result === true){
+                    res.render("secrets");
+                  }
+                });
             }
         }
         
